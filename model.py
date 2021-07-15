e55822624d1b2376d97c1aa14b2984ad428ab9c8
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-07-15 03:48:27 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,29 @@
 import pygad
 from pygad import gann
 
+data = pd.read_csv('data/data1.csv')
 
-GANN_instance = pygad.gann.GANN(num_solutions=6,
-                                num_neurons_input=2,
+X= data.drop('y',axis=1)
+Y= data.y
+
+model = None
+
+# Preparing the NumPy array of the inputs.
+data_inputs = numpy.array(X)
+
+# Preparing the NumPy array of the outputs.
+data_outputs = numpy.array(Y)
+
+num_classes = 2
+num_inputs = 64
+
+num_solutions = 6 # number of clients (or network size)
+GANN_instance = pygad.gann.GANN(num_solutions=num_solutions,
+                                num_neurons_input=num_inputs,
                                 num_neurons_hidden_layers=[2],
-                                num_neurons_output=2)
+                                num_neurons_output=num_classes,
+                                hidden_activations=["relu"],
+                                output_activation="softmax")
 
 GANN_instance.population_networks
 print(GANN_instance.population_networks)
